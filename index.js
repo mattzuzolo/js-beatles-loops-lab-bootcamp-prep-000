1bf62d1761e368bfb127de13545a97c95ce6f15d
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-11 23:48:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay (arrayMusicians, arrayInstruments) {
+
+  var myArray = [];
+  
+  for (let i = 0; i < arrayMusicians.length; i++) {
+      myArray[i] = (`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+      console.log((`${arrayMusicians[i]} plays ${arrayInstruments[i]}`))
+  }
+  
+  return myArray;  
+}
+
+function johnLennonFacts (arrayFacts) {
+  
+  var n = 0;
+  
+  while (n++ < arrayFacts.length) {
+    
+    arrayFacts[n] += "!!!"
+    n;
+  }
+  
+  return arrayFacts;
+}
